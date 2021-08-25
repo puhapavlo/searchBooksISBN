8e78cd8aafaa944ac5d65635950dbdeabeaf9054
Pavlo <puhapavlo13@gmail.com>
2021-08-25 15:54:46 +0300

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { allowedNodeEnvironmentFlags } = require("process");
+const CopyPlugin = require("copy-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const SASS = require("sass");
+
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
+
+const optimization = () => {
+  config = {
+    splitChunks: {
+      chunks: "all",
+    },
+  };
+
+  if (isProd) {
+    config.minimize = true;
+    config.minimizer = [new TerserPlugin(), new CssMinimizerPlugin()];
+  }
+
+  return config;
+};
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: {
+    main: ["@babel/polyfill", "./js/main.js"]
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "docs"),
+    clean: true,
+  },
+  optimization: optimization(),
+  devServer: {
+    hot: isDev,
+    port: 9000,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /.s?css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
